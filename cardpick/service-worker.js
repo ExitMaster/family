c94d4e59fd9e127e8242f979c4e8feb22ef93b25
ExitMaster <gomtang@gmail.com>
2026-08-19 15:15:05 +0900

    Cache CardPick v1.5 academy optimizer

--- cardpick/service-worker.js
+++ cardpick/service-worker.js
@@ -1,7 +1,7 @@
-const CACHE='card-pick-v14';
+const CACHE='card-pick-v15';
 const ASSETS=[
   './','./index.html','./styles.css','./v2.css','./app-v2.js','./default-data-v2.js','./engine-v2.js','./engine-v2.js?base=1','./engine-v12.js',
-  './v12-preload.js','./v12-preapp-extra.js','./v12-details.js','./v12-ui.js','./ui-enhancer-v2.js','./benefit-ui-v2.js','./benefit-details-v2.js','./card-images-v2.js','./v13-links.js','./v14-recording.js',
+  './v12-preload.js','./v12-preapp-extra.js','./v12-details.js','./v12-ui.js','./ui-enhancer-v2.js','./benefit-ui-v2.js','./benefit-details-v2.js','./card-images-v2.js','./v13-links.js','./v14-recording.js','./v15-academy-optimizer.js',
   './assets/cards/samsung-happy-v2.webp','./assets/cards/samsung-lawyers.webp','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'
 ];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
